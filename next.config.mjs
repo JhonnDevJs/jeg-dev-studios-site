594faad0141547c308e5407dab8c11c1de2965fe
JhonDev <153306997+JhonEG-17@users.noreply.github.com>
2025-08-05 11:01:15 -0600

    fix: pathname for image blog section

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,7 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'blogger.googleusercontent.com',
         port: '', // Generalmente vacío para puertos estándar (80 para http, 443 para https)
-        pathname: '/img/b/R29vZ2xl/**', // Ajusta el pathname según sea necesario, '**' es un comodín
+        pathname: '/**', // Aún más flexible, permite cualquier ruta en ese hostname.
       },
     ],
   },
