423662e03390651bffbffb1b674393417bb912c3
JhonEG-17 <jhonataneg@outlook.com>
2025-10-16 20:52:15 -0600

    feat: add links for new profiles to the social network

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -4,11 +4,11 @@ function Footer() {
   return (
     <footer className="d-flex flex-column justify-content-center align-items-center bg-black text-white w-100 p-3" role="contentinfo">
       <p className="fs-5 text-white text-center mt-5">
-        Ya sea que estés comparando <strong>agencias de diseño</strong> o buscando una <strong>agencia de desarrollo web</strong> que entienda las necesidades de tu negocio, en JEG Dev Studios encontrarás una empresa que combina creatividad, tecnología y estrategia.
+        Ya sea que estés comparando <strong>agencias de diseño</strong> o buscando una <strong>agencia de desarrollo web</strong> que entienda las necesidades de tu negocio, en JEG Dev studios encontrarás una empresa que combina creatividad, tecnología y estrategia.
       </p>
       <div className="d-flex flex-xl-row flex-column justify-content-center align-items-center w-100 gap-xl-5 gap-3">
         <article className="d-flex flex-column justify-content-center align-items-center h-100">
-          <Link href="/sobre-nosotros" className="text-decoration-none text-white fs-4" tabIndex={0} title="Página sobre nosotros y la empresa jeg dev studios" aria-label="Página sobre nosotros y la empresa jeg dev studios" aria-labelledby="sobre-nosotros">
+          <Link href="/sobre-nosotros" className="text-decoration-none text-white fs-4" tabIndex={0} title="Página sobre nosotros y la empresa JEG Dev studios" aria-label="Página sobre nosotros y la empresa JEG Dev studios" aria-labelledby="sobre-nosotros">
             ¿Quiénes somos?
           </Link>
         </article>
@@ -17,8 +17,8 @@ function Footer() {
             href="/aviso-de-privacidad"
             className="text-decoration-none text-white fs-4"
             tabIndex={0}
-            title="Aviso de privacidad de jeg dev studios"
-            aria-label="Aviso de privacidad de jeg dev studios"
+            title="Aviso de privacidad de JEG Dev studios"
+            aria-label="Aviso de privacidad de JEG Dev studios"
             aria-labelledby="aviso-de-privacidad"
           >
             Aviso de privacidad
@@ -29,8 +29,8 @@ function Footer() {
             href="/terminos-y-condiciones"
             className="text-decoration-none text-white fs-4"
             tabIndex={0}
-            title="Términos y condiciones de jeg dev studios"
-            aria-label="Términos y condiciones de jeg dev studios"
+            title="Términos y condiciones de JEG Dev studios"
+            aria-label="Términos y condiciones de JEG Dev studios"
             aria-labelledby="terminos-y-condiciones"
           >
             Términos y condiciones
@@ -45,8 +45,8 @@ function Footer() {
                 href="https://www.facebook.com/JEGDevStudios"
                 target="_blank"
                 rel="noopener noreferrer"
-                title="Enlace a página de facebook"
-                aria-label="Enlace a página de facebook"
+                title="Enlace a página de facebook de JEG Dev studios"
+                aria-label="Enlace a página de facebook de JEG Dev studios"
                 aria-labelledby="facebook"
                 tabIndex={0}
               ></Link>
@@ -55,9 +55,29 @@ function Footer() {
                 href="https://instagram.com/jegdevstudios/"
                 target="_blank"
                 rel="noopener noreferrer"
-                title="Enlace a página de Instagram"
-                aria-label="Enlace a página de Instagram"
+                title="Enlace a perfil de Instagram de JEG Dev studios"
+                aria-label="Enlace a perfil de Instagram de JEG Dev studios"
                 aria-labelledby="instagram"
+                tabIndex={0}
+              ></Link>
+              <Link
+                className="d-flex fs-2 text-decoration-none icon-tiktok __icons-color"
+                href="https://www.tiktok.com/@jeg.dev.studios"
+                target="_blank"
+                rel="noopener noreferrer"
+                title="Enlace a perfil de TikTok de JEG Dev studios"
+                aria-label="Enlace a perfil de TikTok de JEG Dev studios"
+                aria-labelledby="TikTok"
+                tabIndex={0}
+              ></Link>
+              <Link
+                className="d-flex fs-2 text-decoration-none icon-youtube __icons-color"
+                href="https://www.youtube.com/@jegdevstudios"
+                target="_blank"
+                rel="noopener noreferrer"
+                title="Enlace a canal de Youtube de JEG Dev studios"
+                aria-label="Enlace a canal de Youtube de JEG Dev studios"
+                aria-labelledby="Youtube"
                 tabIndex={0}
               ></Link>
               <Link
@@ -65,8 +85,8 @@ function Footer() {
                 href="https://linkedin.com/company/jegdevstudios"
                 target="_blank"
                 rel="noopener noreferrer"
-                title="Enlace a página de linkedin"
-                aria-label="Enlace a página de linkedin"
+                title="Enlace a perfil de linkedin de JEG Dev studios"
+                aria-label="Enlace a perfil de linkedin de JEG Dev studios"
                 aria-labelledby="linkedin"
                 tabIndex={0}
               ></Link>
@@ -75,8 +95,8 @@ function Footer() {
                 href="https://github.com/JEGDevStudios"
                 target="_blank"
                 rel="noopener noreferrer"
-                title="Enlace a página de github"
-                aria-label="Enlace a página de github"
+                title="Enlace a perfil de github de JEG Dev studios"
+                aria-label="Enlace a perfil de github de JEG Dev studios"
                 aria-labelledby="github"
                 tabIndex={0}
               ></Link>
@@ -85,7 +105,7 @@ function Footer() {
         </article>
       </div>
       <p className="d-flex justify-content-center align-items-center text-center w-100 mb-0 mt-3">
-        &copy; {new Date().getFullYear()} JEG Dev Studios.<br/>Todos los derechos
+        &copy; {new Date().getFullYear()} JEG Dev studios.<br/>Todos los derechos
         reservados.
       </p>
     </footer>
